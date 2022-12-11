d19e97e9f0a7f8c510a4e7136b91cc072da6999f
Gface4 <kennetgost@gmail.com>
2022-12-11 07:04:49 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>                                                                                                                      
-
 /**
  * main - main block
- *
- * Return: 0
+ * Return: o
  */
 int main(void)
 {
-	puts("\"Programming is like develop a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
