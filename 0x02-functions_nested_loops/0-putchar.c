a1f3e54db8d20df5d0983cb1185467a303439ce8
Gface4 <kennetgost@gmail.com>
2022-12-13 10:40:10 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * main - print _putchar
- * is written by wave
+ * is written by bwave
  * Return: 0
  */
+
 int main(void)
 {
 _putchar('_');
@@ -14,6 +16,6 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
-_putchar('/n');
+_putchar('\n');
 return (0);
 }
