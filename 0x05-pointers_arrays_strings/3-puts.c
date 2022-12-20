ef304ad63a4510ca14ae196fba9358689a9848a0
Gface4 <kennetgost@gmail.com>
2022-12-20 09:55:23 -0800

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - print a string to the stdout
+ * @str: the string
+ * Return the lenght of the string
+ */
+
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (stra[a] != '\10')
+	{
+		_putchar(str[a];
+		a++
+	}
+	_putchar('\n');
+}
