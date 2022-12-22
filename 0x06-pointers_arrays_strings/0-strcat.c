63f50c3e967e8cc6d207862a1223f184c1c64110
Gface4 <kennetgost@gmail.com>
2022-12-22 14:44:30 -0800

    0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	While(dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	While(src[j] != '\0')
+	{
+		des[i] = scr[j];
+		i++;
+		j++;
+	}
+
+	des[i] = '\0';
+	return (dest);
+}
